82f944fc3b0321dc10101d627dedc7e6dffd19b6
Vincent Zou <vincent.zou@ullink.com>
2016-05-11 14:51:03 +0800

    finish merge sort

--- src/main/java/com/interview/algorithms/sorting/impls/MergeSort.java
+++ src/main/java/com/interview/algorithms/sorting/impls/MergeSort.java
@@ -8,11 +8,55 @@ import com.interview.algorithms.sorting.Sorting;
  * @author: Vincent
  * @since:10:20 PM, 10/5/16
  */
-public class MergeSort<T extends Comparable> extends Sorting<T> {
-
-
+public class MergeSort<T extends Comparable> extends Sorting<T>
+{
+    private T[] cache;
     @Override
-    public void sort(T[] values) {
+    public void sort(T[] values)
+    {
+        cache = (T[]) new Comparable[values.length];
+        sort(values, 0, values.length - 1);
+    }
 
+    public void sort(T[] values, int low, int high)
+    {
+        if (high <= low)
+        {
+            return;
+        }
+
+        int mid =  (high + low) / 2;
+
+        sort(values, low, mid);
+        sort(values, mid + 1, high);
+        merge(values, low, mid, high);
+    }
+
+    public void merge(T[] values, int low, int mid, int high)
+    {
+        int start1 = low;
+        int start2 = mid + 1;
+
+        System.arraycopy(values, low, cache, low, high - low + 1);
+        for (int i = low; i <= high; i++)
+        {
+            if (start1 > mid)
+            {
+                values[i] = cache[start2++];
+            }
+            else if (start2 > high)
+            {
+                values[i] = cache[start1++];
+            }
+            // must use 'cache' array to fetch values as 'values' array is keeping changing with growth of i, the data would be obsolete if read 'values' array
+            else if (cache[start1].compareTo(cache[start2]) < 0)
+            {
+                values[i] = cache[start1++];
+            }
+            else
+            {
+                values[i] = cache[start2++];
+            }
+        }
     }
 }
--- src/test/java/com/interview/algorithms/sorting/SortingTest.java
+++ src/test/java/com/interview/algorithms/sorting/SortingTest.java
@@ -1,19 +1,20 @@
 package com.interview.algorithms.sorting;
 
+import java.util.Collections;
+import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import com.google.common.base.Joiner;
-import com.google.common.collect.*;
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Range;
 import com.interview.algorithms.princeton.CommonTest;
 import com.interview.algorithms.sorting.impls.InsertionSort;
 import com.interview.algorithms.sorting.impls.MergeSort;
 import com.interview.algorithms.sorting.impls.SelectionSort;
 import com.interview.algorithms.sorting.impls.ShellSort;
-import jdk.nashorn.internal.scripts.JO;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import java.util.Collections;
-import java.util.List;
 
 
 /**
@@ -46,8 +47,8 @@ public class SortingTest extends CommonTest {
         return new Object[][]{
             {"Sorting algorithms, Selection Sort", new SelectionSort<Integer>()},
             {"Sorting algorithms, Insertion Sort", new InsertionSort<Integer>()},
-            {"Sorting algorithms, Shell Sort with 3-sort", new ShellSort<Integer>()}
-//            {"Sorting algorithms, Selection Sort", new MergeSort<Integer>()},
+            {"Sorting algorithms, Shell Sort with 3-sort", new ShellSort<Integer>()},
+            {"Sorting algorithms, Merge Sort", new MergeSort<Integer>()},
         };
     }
 }
