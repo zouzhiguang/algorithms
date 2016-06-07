001cc78f16e52525c45a8cd1416ad376ea489db1
Vincent Zou <vincent.zou@ullink.com>
2016-06-07 18:26:11 +0800

    implement msd and 3-quick string sort.

--- src/main/java/com/interview/algorithms/strings/impls/MSDSorting.java
+++ src/main/java/com/interview/algorithms/strings/impls/MSDSorting.java
@@ -1,7 +1,5 @@
 package com.interview.algorithms.strings.impls;
 
-import java.util.List;
-import com.interview.algorithms.strings.IndexCountingSort.StringIndexSort;
 import com.interview.algorithms.strings.StrirngSorting;
 
 /**
@@ -11,17 +9,86 @@ import com.interview.algorithms.strings.StrirngSorting;
  */
 public class MSDSorting implements StrirngSorting
 {
+    private static final int DEFAULT_CUT_OFF = 15; // if string sub-array length is smaller than cut-off, would use insertion sort to sort the elements.
+    private static final int R = 256;
+
     @Override
     public String[] sort(String[] strings, int length)
     {
-        StringIndexSort stringIndexSort = new StringIndexSort(255, strings.length);
-        for (int i = 0; i < length; i++){
-            stringIndexSort.sort(strings, i);
-            int[] counts = stringIndexSort.getCounts();
-            // split the counts into sub arrays, then sort the sub-array accordingly.
+        // for MSD, the length is not a constraints as it can sort strings with different length.
+        return sort(strings);
+    }
 
+    private String[] sort(String[] sources)
+    {
+        sort(sources, 0, sources.length -1, 0, new String[sources.length]);
+        return sources;
+    }
+
+    private void sort(String[] sources, int low, int high, int digit, String[] aux){
+        if(high < low  + DEFAULT_CUT_OFF){
+            insertionSort(sources, low, high);
+            return;
+        }
+
+        // count[0] used for marking of the end of strings,
+        // count[1 ~ R+1] is used for position indexing.
+        int[] count = new int[R + 2];
+
+        // firstly count
+        for(int i = low; i <= high; i++){
+            count[charAt(sources[i], digit) + 2]++;
+        }
+
+        // add up to index
+        for(int i = 1; i < R+2; i++){
+            count[i] += count[i-1];
+        }
+
+        // insert into aux
+        for (int i = low; i <= high; i++ ){
+            aux[count[charAt(sources[i], digit)]++] = sources[i];
+        }
+
+        // copy back
+        for(int i = low; i <= high; i++){
+            sources[i] = aux[i];
+        }
+
+        // 0 is for null character already, do need to loop again?
+        for (int r = 1; r < R; r++){
+            sort(sources, low + count[r], low + count[r+1] - 1, digit + 1, aux);
         }
     }
 
+    private int charAt(String s, int digit){
+        if(digit >= s.length()){
+            // already index out of array, would treat it as -1 to put at the very start of the array.
+            return -1;
+        }
+
+        return s.charAt(digit);
+    }
+
+    private void insertionSort(String[] sources, int low, int high)
+    {
+        for (int i = low; i <= high; i++)
+        {
+            for (int j = i; j > low; j--)
+            {
+                if (sources[j].compareTo(sources[j - 1]) < 0)
+                {
+                    swap(sources, j, j - 1);
+                }
+            }
+        }
+    }
+
+    private void swap(String[] sources, int j, int i)
+    {
+        String tmp = sources[i];
+        sources[i] = sources[j];
+        sources[j] = tmp;
+    }
 
 }
---
+++ src/main/java/com/interview/algorithms/strings/impls/ThreeWayQuickMSD.java
@@ -0,0 +1,16 @@
+package com.interview.algorithms.strings.impls;
+
+/**
+ * use 3-way quick sort to partition the sub array into three part: smaller than the pivot, equal to the pivot, larger than the pivot by swap and keep the margin using small, greater position.
+ *
+ * It's using quick sort algorithm rather than traditional MSD which is using index array counting.
+ *
+ * As it's using comparison between strings, so the average performance should be lower bounded by 2NLogN compares.
+ *
+ * @author: vzou
+ * *
+ * @since: 6/7/2016.
+ */
+public class ThreeWayQuickMSD
+{
+}
